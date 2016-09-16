c6fa10e6827cca31098b94957c9d7b652e40ccae
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-09-16 17:48:42 -0400

    sass

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 
-	<title></title>
+	<title>In-Office Dental Implant Guide System | GuidePro3D</title>
+	<meta name="description" content="GuidePro3D offers the most innovative, simple-to-use dental implant guide system, printed in-office that can be used with or without a CBCT scan.">
 
 	<link rel="icon" type="image/png" href="img/favicon.png">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 
+<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
