45a41d0cf8a332f2f797bd58a498a1eb4b981b70
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-09-16 17:34:53 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+
+	<title></title>
+
+	<link rel="icon" type="image/png" href="img/favicon.png">
+</head>
+<body>
+
+</body>
+</html>
