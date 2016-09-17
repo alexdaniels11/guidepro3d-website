b55aad29e3bcaf36018e1c475de91cbb899b7a81
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-09-17 18:05:44 -0400

    header.php

---
+++ elements/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<title>In-Office Dental Implant Guide System | GuidePro3D</title>
+	<meta name="description" content="GuidePro3D offers the most innovative, simple-to-use dental implant guide system, printed in-office that can be used with or without a CBCT scan.">
+
+	<link rel="icon" type="image/png" href="img/favicon.png">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="sha384-2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,400i,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+
+
+
+
+
+<header>
+	<div class="container">
+		<div class="header-logo">
+			<a href="/"><img src="img/web-logo.png"></a>
+		</div>
+		<!-- begin navigation -->
+		<nav class="main-navigation">
+			<div class="menu-btn">
+				<a href="#">Request a demo</a>
+			</div>
+			<ul>
+				<li><a href="#">The GuidePro<sup>3D</sup> System</a></li>
+				<li><a href="#">Events</a></li>
+				<li><a href="#">Resource Center</a></li>
+				<li><a href="#">Our Story</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+<div class="container">
+	<div class="quick-nav">
+		<ul>
+			<span class="pull-xs-right">
+				<li><a class="header-search" href="#"><span class="search-icon"></span> Search</a></li>
+				<li><a class="header-login" href="#"><span class="login-icon"></span> Login</a></li>
+			</span>
+		</ul>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+<?php include ('elements/header.php'); ?>
 
-	<title>In-Office Dental Implant Guide System | GuidePro3D</title>
-	<meta name="description" content="GuidePro3D offers the most innovative, simple-to-use dental implant guide system, printed in-office that can be used with or without a CBCT scan.">
 
-	<link rel="icon" type="image/png" href="img/favicon.png">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
-<body>
 
-<script type="text/javascript" src="js/main.js"></script>
-</body>
-</html>
+
+
+
+<?php include ('elements/footer.php'); ?>
