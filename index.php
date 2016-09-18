2bf3b9f9e0bc375df79b89ac321e1813d8584db6
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-09-18 10:52:07 -0400

    hero-area

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php include ('elements/header.php'); ?>
 
 
-
-
+<div class="hero-area">
+	<div class="container">
+		<div class="hero-text">
+			<h1>The Dental Implant Guide System that Revolutionizes the way Implant Surgery is Done.</h1>
+		</div>
+	</div>
+</div>
 
 
 <?php include ('elements/footer.php'); ?>
