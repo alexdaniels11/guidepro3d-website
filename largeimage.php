b216f1cf37bd87bcfaa75a42341befdacd4924b5
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-10-11 20:57:35 -0400

    large image

--- largeimage.php
+++ largeimage.php
@@ -15,11 +15,61 @@
 		<div class="large-img img-responsive">
 			<img src="img/peeps.jpg">
 		</div>
-		<p>Founded by Dr. Fred Thompson, Dr. Paul Crandall and dental office manager Jennifer Thompson, the goal of GuidePro3D has always been to provide a better standard of care for patients with less stress for dentists.</p>
+		<div class="col-xs-12">
+			<p>Founded by Dr. Fred Thompson, Dr. Paul Crandall and dental office manager Jennifer Thompson, the goal of GuidePro3D has always been to provide a better standard of care for patients with less stress for dentists.</p>
+		</div>
 		<div class="gp-btn row">
 			<p>
 				<a href="#">Meet GuidePro3D’s Founders</a>
 			</p>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="col-lg-7">
+		<div class="large-img-callout-container row">
+			<div class="large-img-callout-text">
+				<p>With more than 10 years of experience in placing implants and teaching dentists how to place implants, Dr. Thompson came up with an idea for mechanical ways to derive The Perfect Restorative Trajectory for implant surgery. After further research and sharing his idea with his dental school classmate and friend, Dr. Crandall, the pair evolved the idea for its betterment – a software program that enables dentists to plan an implant case and print that “recipe” on an in-office 3D printer – GuidePro3D.</p>
+			</div>
+		</div>
+	</div>
+	<div class="large-img-callout-img col-lg-5">
+		<div class="img-responsive row">
+			<img src="img/fred.jpg">
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="mission">
+		<div class="col-sm-5 col-xs-12">
+			<h2>Our Mission</h2>
+			<p>GuidePro3D has always had five goals</p>
+		</div>
+		<div class="col-sm-7 col-xs-12 mission-numbers">
+			<ol>
+				<li>Create a product that is inexpensive to produce</li>
+				<li>Print in office</li>
+				<li>Easy for dental offices to implement into their workflow</li>
+				<li>Simple to verify in-office</li>
+				<li>Can be done with or without a CBCT scan</li>
+			</ol>
+		</div>
+	</div>
+</div>
+<div class="container mission-statements">
+	<div class="col-md-4 col-xs-12">
+		<div class="mission-statement">
+			<p>The product has changed dramatically since the inception of the idea, but those goals have always remained constant along with concept of The Perfect Restorative Trajectory.</p>
+		</div>
+	</div>
+	<div class="col-md-4 col-xs-12">
+		<div class="mission-statement">
+			<p>GuidePro3D’s mission is to provide a consistently accurate method of placing dental implants in The Perfect Restorative Trajectory in an easy-to-use, economical manner that instills confidence in the dentists who use it.</p>
+		</div>
+	</div>
+	<div class="col-md-4 col-xs-12">
+		<div class="mission-statement">
+			<p>Knowing what we know now, we would never place an implant without a guide. Thus our system now makes it affordable and easy to do surgery “guided every time.”</p>
 		</div>
 	</div>
 </div>
